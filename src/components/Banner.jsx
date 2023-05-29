f46175de84ee6abb67d3b627f937c2311c3e8d4e
Laure <quanelle@gmail.com>
2023-05-29 17:44:24 +0200

    position footer importation police

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import '../style/banner.css'
-// import picture from '../utils/pictures'
+import picture from '../utils/pictures'
+import styled from 'styled-components';
 
+// const Bannerstyle = styled.div`
+// background-color: #F6F6F6;
+//     background: url('/src/assets/home.png');
+//    background:url(
+//       ${({ adress }) =>
+//     adress === '/' ? picture.homeImg : picture.aproposImg};)
+    
+//     background-size:cover;
+//     font-family: 'Montserrat';
+// font-style: normal;
+// font-weight: 500;
+// font-size: 48px;
+// line-height: 142.6%;
 
-export default function Banner() {
+// /* or 68px */
+// display: flex;
+// align-items: flex-end;
+// justify-content: center;
+
+// color: #FFFFFF;
+// }
+// `
+
+export default function Banner({image}) {
+  
   return (
     <div className='banner'>Chez vous, partout et ailleurs</div>
+    // <Bannerstyle>Chez vous, partout et ailleurs</Bannerstyle>
   )
 }
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import '../style/error.css'
 
-import Header from '../components/Header'
 
 export default function Error() {
   return (
     <>
     
     <div className='page-container'>
+    <div className='error'>
     <div className='chiffre'>404</div>
     <div className="text-erreur">Oups! La page que vous demandez n'existe pas.</div>
     <div className="text-retour-accueil">Retourner sur la page d’accueil</div>
     </div>
+    </div>
+    
     </>
   )
 }
